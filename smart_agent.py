1754f656619f460396b98cdf5d89ace8beba78fe
zzlgreat <zzlgreat@gmail.com>
2023-09-24 11:11:21 +0800

    add some factors

--- smart_agent.py
+++ smart_agent.py
@@ -1,5 +1,6 @@
 from solver import solve_plan_work
-
+# example:
+#user_prompt = "what movies did the director of 'Killers of the Flower Moon' direct？ List one of them and search it in bilibili."
 # input
 user_prompt = input("Please enter your query: ")
 user_prompt += "\n"
